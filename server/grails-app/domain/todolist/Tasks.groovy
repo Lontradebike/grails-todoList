be952a6b04bacfb4036582f67e42b27cd97413b1
lontradebike <jmarceloribeiro@hotmail.com>
2021-12-09 16:49:38 -0300

    primeiro commit

---
+++ server/grails-app/domain/todolist/Tasks.groovy
@@ -0,0 +1,11 @@
+package todolist
+
+class Tasks {
+    String nome
+    String descricao
+    String dias
+
+    static constraints = {
+
+    }
+}
---
+++ server/grails-app/services/todolist/TasksService.groovy
@@ -0,0 +1,18 @@
+package todolist
+
+import grails.gorm.services.Service
+
+@Service(Tasks)
+interface TasksService {
+
+    Tasks get(Serializable id)
+
+    List<Tasks> list(Map args)
+
+    Long count()
+
+    Tasks delete(Serializable id)
+
+    Tasks save(Tasks tasks)
+
+}
